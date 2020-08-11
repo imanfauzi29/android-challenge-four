546f747a0ded8213a14da5a8444f172abf820f0c
M Rohmatullah <m.rohmatullah10@gmail.com>
2020-08-11 12:35:47 +0700

    splash and terms

--- app/src/main/java/com/mycorp/javachallengefour/SplashActivity.java
+++ app/src/main/java/com/mycorp/javachallengefour/SplashActivity.java
@@ -2,25 +2,45 @@ package com.mycorp.javachallengefour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 import com.luqman.dev.helloworld.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    public static Boolean terms;
+    public static final String  MY_SHARED = "session_status";
+    public static final String  SESSION_TERMS = "syarat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sharedPreferences = getSharedPreferences(MY_SHARED, Context.MODE_PRIVATE);
+
+        terms = sharedPreferences.getBoolean(SESSION_TERMS, false);
+
+        int ms = 3000;
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, TermActivity.class));
-                finish();
+                if (!terms){
+                    startActivity(new Intent(SplashActivity.this, TermActivity.class));
+                    finish();
+                }else{
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
             }
-        }, 3000);
+        }, ms);
     }
 }
--- app/src/main/java/com/mycorp/javachallengefour/TermActivity.java
+++ app/src/main/java/com/mycorp/javachallengefour/TermActivity.java
@@ -2,8 +2,11 @@ package com.mycorp.javachallengefour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -13,29 +16,74 @@ import com.luqman.dev.helloworld.R;
 
 public class TermActivity extends AppCompatActivity {
 
+    AlertDialog dialog;
+    AlertDialog.Builder alert;
+    CheckBox checkBox;
+    View cbView;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term);
 
-        View cbView = View.inflate(this, R.layout.checkbox, null);
-        CheckBox checkBox = cbView.findViewById(R.id.checkbox);
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        sharedPreferences = getSharedPreferences(SplashActivity.MY_SHARED, Context.MODE_PRIVATE);
 
-            }
-        });
+        cbView = View.inflate(this, R.layout.terms_message, null);
+        checkBox = cbView.findViewById(R.id.checkbox);
 
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("test")
-                .setMessage(R.string.terms_message)
+        alert = new AlertDialog.Builder(this);
+        alert.setTitle("TERMS AND CONDITIONS")
+                .setCancelable(false)
+                .setView(cbView)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean(SplashActivity.SESSION_TERMS,true);
+                        editor.apply();
                     }
-                })
-                .create().show();
+                });
+
+        dialog = alert.create();
+
+//        checkedBox(dialog, checkBox);
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+
+                checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                        if (checkBox.isChecked()) ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                        else ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                    }
+                });
+            }
+        });
+
+        dialog.show();
     }
+
+//    private void checkedBox(final AlertDialog dialog, final CheckBox checkBox) {
+//        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+//            @Override
+//            public void onShow(DialogInterface dialogInterface) {
+//                ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+//
+//                checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//                    @Override
+//                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                        if (checkBox.isChecked()) ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+//                        else ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+//                    }
+//                });
+//            }
+//        });
+//
+//        dialog.show();
+//    }
+
 }
